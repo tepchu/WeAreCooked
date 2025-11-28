c7b2ac28f4b22fe06a39ad7d02b2873d6fb6c6a6
Radhitya27 <18224126@std.stei.itb.ac.id>
2025-11-28 12:54:46 +0700

    init : initialize Direction and Position

---
+++ nimonscooked/src/main/java/models/core/Position.java
@@ -0,0 +1,39 @@
+package models.core;
+
+public class Position {
+
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() { 
+        return x; 
+    }
+
+    public int getY() { 
+        return y; 
+    }
+
+    public void setX(int x) { 
+        this.x = x; 
+    }
+
+    public void setY(int y) { 
+        this.y = y; 
+    }
+
+    public boolean equals(Position p) {
+        if (p == null) return false;
+        return this.x == p.x && this.y == p.y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
+
