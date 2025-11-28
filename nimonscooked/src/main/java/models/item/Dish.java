ed5b119040e45e4fd3ca65e69144c970d9a72d7f
stphniemae <stphaniemae@gmail.com>
2025-11-28 11:32:38 +0700

    add: added dish class

---
+++ nimonscooked/src/main/java/models/item/Dish.java
@@ -0,0 +1,32 @@
+package models.item;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Dish extends Item {
+    private String dishName;
+    private List<Preparable> components;
+
+    public Dish(String dishName) {
+        super(dishName);
+        this.dishName = dishName;
+        this.components = new ArrayList<>();
+    }
+
+    public void addComponent(Preparable ingredient) {
+        components.add(ingredient);
+    }
+
+    public List<Preparable> getComponents() {
+        return new ArrayList<>(components);
+    }
+
+    public String getDishName() {
+        return dishName;
+    }
+
+    @Override
+    public void showItem() {
+        System.out.println("Dish: " + dishName + " currently has " + components.size() + " components");
+    }
+}
