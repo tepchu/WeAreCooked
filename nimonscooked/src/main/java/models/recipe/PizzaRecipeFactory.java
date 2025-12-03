df48ee208d7ebf3ba2e0cb8c7ffa1d7b6b20c247
stphniemae <stphaniemae@gmail.com>
2025-12-03 11:28:44 +0700

    fix: fixed PizzaRecipeFactory for Stage.java

--- nimonscooked/src/main/java/models/recipe/PizzaRecipeFactory.java
+++ nimonscooked/src/main/java/models/recipe/PizzaRecipeFactory.java
@@ -5,6 +5,18 @@ import java.util.List;
 
 public class PizzaRecipeFactory {
 
+    public static Recipe createPizzaMargherita() {
+        return new PizzaMargheritaRecipe();
+    }
+
+    public static Recipe createPizzaSosis() {
+        return new PizzaSosisRecipe();
+    }
+
+    public static Recipe createPizzaAyam() {
+        return new PizzaAyamRecipe();
+    }
+
     public List<Recipe> createAllRecipes() {
         List<Recipe> recipes = new ArrayList<>();
 
@@ -13,6 +25,10 @@ public class PizzaRecipeFactory {
         recipes.add(new PizzaAyamRecipe());
 
         return recipes;
+    }
+
+    public List<Recipe> getAllRecipes() {
+        return createAllRecipes();
     }
 
     public Recipe getRecipeByName(String name) {
